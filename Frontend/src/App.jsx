cf2a450a147111e9ad6d907aa80126f66f055e75
SanyamRwt <166116134+SanyamRwt@users.noreply.github.com>
2026-02-19 13:12:46 +0530

    update api url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   console.log("hello Integration");
 
   function fetchNotes() {
-    axios.get("http://localhost:3000/api/notes").then(res => {
+    axios.get(" https://cohot-2-0-25zo.onrender.com/api/notes").then(res => {
       setNotes(res.data.notes);
     });
   }
@@ -24,7 +24,7 @@ function App() {
     console.log(title.value, description.value);
 
     axios
-      .post("http://localhost:3000/api/notes", {
+      .post(" https://cohot-2-0-25zo.onrender.com/api/notes", {
         title: title.value,
         description: description.value,
       })
@@ -36,7 +36,7 @@ function App() {
   }
 
   function handleDeleteNote(noteId) {
-    axios.delete("http://localhost:3000/api/notes/" + noteId)
+    axios.delete(" https://cohot-2-0-25zo.onrender.com/api/notes/" + noteId)
     .then(res => {
       console.log(res.data);
 
